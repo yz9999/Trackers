1b76876696b27486d262077966c7d2576d147f83
yz9999 <109860348+yz9999@users.noreply.github.com>
2025-06-05 16:27:56 +0800

    Update merge_trackers2.py

--- merge_trackers2.py
+++ merge_trackers2.py
@@ -2,8 +2,7 @@ import requests
 
 sources = [
     "https://cf.trackerslist.com/all.txt",
-    "https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt",
-    "https://raw.githubusercontent.com/Tunglies/TrackersList/main/all.txt"
+    "https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt"
 ]
 
 def fetch_trackers(url):
